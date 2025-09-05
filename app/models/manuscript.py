a3687056f9a8132eacb26f358682003e7a5b3a8c
Anissa667u <mollasta@mail.ru>
2025-09-05 19:16:21 +0500

    код
    код

--- app/models/manuscript.py
+++ app/models/manuscript.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from app.core.database import Base
+from sqlalchemy import Date, DateTime 
+import datetime
 
 class ManuscriptModel(Base):
     __tablename__ = "Manuscripts"
@@ -8,8 +10,21 @@ class ManuscriptModel(Base):
     author: Mapped[str]
     description: Mapped[str]
     
-'''class MSchapterModel(ManuscriptModel):
+class MSchapterIDModel(ManuscriptModel):
     __tablename__ = "Manuscript's Chapter"
     chapter_id: Mapped[int] = mapped_column(primary_key=True)
-    chapter_title: Mapped[str]
-    date_of_publication: [date]'''
+    comments: ....
+
+class MSChapterModel(MSchapterIDModel)
+    chapter_title: Mapped[str]  
+    date_of_publication: Mapped[datetime.datetime] = mapped_column(DateTime) 
+    content: Mapped[str]
+    
+
+class ChapterCommentsModel(MSchapterIDModel):
+    __tablename__ = "Comments for chapter"
+    comment_id: Mapped[int] = mapped_column(primary_key= True)
+    content : Mapped[str]
+    date_posted: Mapped[datetime.date] = mapped_column(Date)
+    chapter_id: Mapped[int] = mapped_column(ForeignKey("chapters.chapter_id"))    
+    
