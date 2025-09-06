c369e3344bd2418ccda7b33f4d7d9659ec3e3c1c
Anissa667u <mollasta@mail.ru>
2025-09-06 15:24:01 +0500

    изменение
    написала валидаций данных на пайдентике для manuscripts, chapters amd comments

--- app/schemas/manuscript.py
+++ app/schemas/manuscript.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+import datetime
 
 class MSAddSchema(BaseModel):
     title:str 
@@ -10,3 +11,26 @@ class MSSchema(MSAddSchema):
     
     class Config:
         orm_mode = True 
+        
+class MSAddChapter(BaseModel): #Валидация данных для глав
+    chapter_title: str
+    content: str 
+    manuscript_id: int 
+    date_of_publication: datetime.datetime 
+    
+class MS_chapter(MSAddChapter):
+    chapter_id: int 
+    class Config:
+        orm_mode = True 
+        
+class MSAddChapterComment(BaseModel):
+    content: str
+    chapter_id: int 
+    
+class MSChapterComment(MSAddChapterComment):
+    date_posted: datetime 
+    comment_id:int 
+    class Config:
+        orm_mode = True
+    
+    
